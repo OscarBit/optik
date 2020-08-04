4ed39bc371593dd868d3725345089888f8605a72
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-08-03 20:49:53 -0500

    Fix test_optik2 file

--- test_optik2.py
+++ test_optik2.py
@@ -2,16 +2,17 @@ import unittest
 import numpy as np
 import optik2 as optik
 
-class TestOptik(unittest.TestCase):
 
+class TestOptik(unittest.TestCase):
     def test_legos(self):
-        layer = optik.lego('test_layer',filename='Si.txt')
+        layer = optik.lego("test_layer", file_name="Si.txt")
         data = np.genfromtxt("./Materials/Si.txt")
         data = data.T
         "Test with Si.txt file already prepare"
-        self.assertEqual(data[0], layer.lambd)
-        self.assertEqual(data[1], layer.n)
-        self.assertEqual(data[2], layer.k)
+        assert np.allclose(data[0], layer.lambd)
+        assert np.allclose(data[1], layer.n)
+        assert np.allclose(data[2], layer.k)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
