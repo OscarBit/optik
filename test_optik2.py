5a4978f866385793ea804672dd84ba90db0627d4
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-08-03 20:57:42 -0500

    Change test in order to use pytest

--- test_optik2.py
+++ test_optik2.py
@@ -1,18 +1,12 @@
-import unittest
-import numpy as np
+import numpy
 import optik2 as optik
 
 
-class TestOptik(unittest.TestCase):
-    def test_legos(self):
-        layer = optik.lego("test_layer", file_name="Si.txt")
-        data = np.genfromtxt("./Materials/Si.txt")
-        data = data.T
-        "Test with Si.txt file already prepare"
-        assert np.allclose(data[0], layer.lambd)
-        assert np.allclose(data[1], layer.n)
-        assert np.allclose(data[2], layer.k)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_legos():
+    layer = optik.lego("test_layer", file_name="Si.txt")
+    data = numpy.genfromtxt("./Materials/Si.txt")
+    data = data.T
+    "Test with Si.txt file already prepare"
+    assert numpy.allclose(data[0], layer.lambd)
+    assert numpy.allclose(data[1], layer.n)
+    assert numpy.allclose(data[2], layer.k)
